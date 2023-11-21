2028e0eb48e7bd0f4be6b2d23813678bac10e3c5
stargeaze <yasminfsalma@gmail.com>
2023-11-21 19:45:04 +0700

    update htmlParser (add input in function)

--- src/htmlParser.py
+++ src/htmlParser.py
@@ -1,40 +1,41 @@
 from os import system
 
-def parseHTML(file_path):
+def parseHTML():
     parsedHTML = []
-    
-    with open(file_path, 'r') as file:
-        lines = file.readlines()
-        
-        for line_number, line in enumerate(lines, 1):
-            tag = ''
-            in_tag = False  
-            
-            for char in line:
-                if char == '<':  
-                    in_tag = True
-                    tag += '<' 
-                elif char == '>':  
-                    tag += '>'
-                    parsedHTML.append((line_number, tag))
-                    tag = '' 
-                    in_tag = False
-                elif in_tag: 
-                    tag += char
-                    
-    return parsedHTML
+    file_path = input("Input file path: ")
 
-
-while True:
     try:
-        file_path = input("Input file path: ")
-        result = parseHTML(file_path)
-        for r in result:
-            print(r)
-        break 
+        with open(file_path, 'r') as file:
+            lines = file.readlines()
+
+            for line_number, line in enumerate(lines, 1):
+                tag = ''
+                in_tag = False  
+
+                for char in line:
+                    if char == '<':  
+                        in_tag = True
+                        tag += '<' 
+                    elif char == '>':  
+                        tag += '>'
+                        parsedHTML.append((line_number, tag))
+                        tag = '' 
+                        in_tag = False
+                    elif in_tag: 
+                        tag += char
+        return parsedHTML
+    
     except FileNotFoundError:
-        print("File not found. Please try again.") 
+        print("File not found. Please try again.")
         system('pause')
         system('cls')
+        return None
+    
     except Exception as e:
-        print(f"An error occurred: {e}")  
+        print(f"An error occurred: {e}")
+        return None
+
+result = parseHTML()
+if result:
+    for r in result:
+        print(r)
