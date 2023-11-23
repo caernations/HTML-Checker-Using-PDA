eeffb29bd35b9931def415c4b9f49c70f081b072
stargeaze <yasminfsalma@gmail.com>
2023-11-23 09:32:06 +0700

    fix parsing

--- src/htmlParser.py
+++ src/htmlParser.py
@@ -1,67 +1,65 @@
-from os import system
-
 def parseHTML():
     parsedHTML = []
     file_directory = "../test/"
 
-    def process_tag(tag):
-        parts = tag.split()
-        tag_name = parts[0]
-        attributes = []
+    def process_tag(tag, line_number):
+        if tag.startswith('<!--'):
+            parsedHTML.append((line_number, '<!---->'))
+            return
 
-        for part in parts[1:]:
-            attr = part.split('=')[0]
-            if '=' in part and attr not in ['method', 'action', 'type']:
-                attributes.append(f'{attr}=""')
-            else:
-                attributes.append(part)
+        if tag.startswith('</'):
+            parsedHTML.append((line_number, tag))
+            return
 
-        attr_str = ' '.join(attributes).rstrip('>')
-        return (tag_name, attr_str if attr_str else None, '>' if attr_str else None)
+        space_index = tag.find(' ')
+        close_index = tag.find('>')
 
-    while True:
-        print("HTML Parser Tool")
-        file_name = input("Input file name: ")
-        file_path = file_directory + file_name
+        if 0 < space_index < close_index:
+            tag_name = tag[1:space_index]
+            attrs = tag[space_index + 1:close_index].strip()
+            parsedHTML.append((line_number, f'<{tag_name}'))
+            for attr in attrs.split():
+                attr_name, equal, attr_value = attr.partition('=')
+                if attr_name.lower() in ['method', 'action', 'type'] and equal:
+                    parsedHTML.append((line_number, f'{attr_name}={attr_value}'))
+                elif equal: 
+                    parsedHTML.append((line_number, f'{attr_name}=""'))
+        else:
+            tag_name = tag[1:close_index]
+            parsedHTML.append((line_number, f'<{tag_name}'))
 
-        try:
-            with open(file_path, 'r') as file:
-                lines = file.readlines()
+        parsedHTML.append((line_number, '>'))
 
-                for line_number, line in enumerate(lines, 1):
-                    tag = ''
-                    in_tag = False
-                    in_comment = False
+    print("HTML Parser Tool")
+    file_name = input("Input file name: ")
+    file_path = file_directory + file_name
 
-                    for char in line:
-                        if char == '<':
-                            tag += '<'
-                            in_tag = True
-                        elif char == '>':
-                            tag += '>'
-                            if in_tag:
-                                if tag.startswith('<!--'):
-                                    in_comment = True
-                                    parsedHTML.append((line_number, '<!---->'))
-                                elif in_comment and tag.endswith('-->'):
-                                    in_comment = False
-                                    parsedHTML.append((line_number, '<!---->'))
-                                else:
-                                    in_tag = False
-                                    tag_name, attrs, closing = process_tag(tag)
-                                    parsedHTML.append((line_number, tag_name))
-                                    if attrs:  
-                                        parsedHTML.append((line_number, attrs))
-                                    if closing is not None: 
-                                        parsedHTML.append((line_number, closing))
-                                    tag = ''
-                        elif in_tag:
-                            tag += char
+    try:
+        with open(file_path, 'r') as file:
+            lines = file.readlines()
 
-            return parsedHTML
+            line_number = 1
+            tag = ''
+            in_tag = False
+            for line in lines:
+                for char in line:
+                    if char == '\n':
+                        line_number += 1
+                    elif char == '<':
+                        in_tag = True
+                        tag = '<'
+                    elif char == '>' and in_tag:
+                        tag += '>'
+                        process_tag(tag, line_number)
+                        tag = ''
+                        in_tag = False
+                    elif in_tag:
+                        tag += char
 
-        except FileNotFoundError:
-            print("File not found. Please try again.")
+        return parsedHTML
 
-        except Exception as e:
-            print(f"An error occurred: {e}")
+    except FileNotFoundError:
+        print("File not found. Please try again.")
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
