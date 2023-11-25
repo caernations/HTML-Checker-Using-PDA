e769da952499ca2e0d9baca26eb48615af308d32
stargeaze <yasminfsalma@gmail.com>
2023-11-25 20:32:39 +0700

    fix html parser

--- src/htmlParser.py
+++ src/htmlParser.py
@@ -1,6 +1,6 @@
-def parseHTML(filename):
+def parseHTML():
     parsedHTML = []
-    file_path = "../test/" + filename
+    file_directory = "../test/"
 
     def process_tag(tag, line_number):
         if tag.startswith('<!--'):
@@ -12,33 +12,40 @@ def parseHTML(filename):
             return
 
         if tag.startswith('</'):
-            parsedHTML.append((line_number, tag))
+            tag = tag.replace('\n', '').replace(' ', '').replace('\t', '')
+            parsedHTML.append((line_number, (tag).lower()))
             return
 
         space_index = tag.find(' ')
         close_index = tag.find('>')
 
         if 0 < space_index < close_index:
-            tag_name = tag[1:space_index]
-            attrs = tag[space_index + 1:close_index].strip()
-            parsedHTML.append((line_number, f'<{tag_name}'))
+            tag_name = (tag[1:space_index]).strip()
+            attrs = tag[space_index + 1:close_index]
+            parsedHTML.append((line_number, (f'<{(tag_name.strip(" ")).lower()}')))
             for attr in attrs.split():
                 attr_name, equal, attr_value = attr.partition('=')
-                if attr_name.lower() in ['method', 'action', 'type'] and equal:
-                    parsedHTML.append((line_number, f'{attr_name}={attr_value}'))
-                elif equal: 
-                    parsedHTML.append((line_number, f'{attr_name}=""'))
+                if attr_name.lower() in ['method', 'type'] and equal:
+                    parsedHTML.append((line_number, f'{(attr_name).lower()}={(attr_value).lower()}'))
+                elif not equal and attr_name: 
+                    parsedHTML.append((line_number, f'{(attr_name).lower()}'))
+                else:  
+                    parsedHTML.append((line_number, f'{(attr_name).lower()}=""'))
         else:
             tag_name = tag[1:close_index]
-            parsedHTML.append((line_number, f'<{tag_name}'))
+            parsedHTML.append((line_number, f'<{(tag_name).lower()}'))
 
         parsedHTML.append((line_number, '>'))
+
+
+    file_name = input("Input file name: ")
+    file_path = file_directory + file_name
 
     try:
         with open(file_path, 'r') as file:
             lines = file.readlines()
             line_number = 1
-            tag = ''
+            (tag) = ''
             in_tag = False
             for line in lines:
                 for char in line:
