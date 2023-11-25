922d4fa6def20773e2de544f2da10eaa0bb5c0b6
stargeaze <yasminfsalma@gmail.com>
2023-11-25 20:32:53 +0700

    restore main

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 from os import system
 import htmlParser
 import sys
-import pda
+
+
 while True:
-    system('cls')
     print("""
               
 ═════════════════════════════════════════
@@ -40,56 +40,54 @@ while True:
     system('pause')
     system('cls')
     if (chooseMenu == '1') or (chooseMenu == 'CHECK HTML'):
-        print("HTML Parser")
-        html_file_name = input("Input HTML filename (.html): ")
-        html_input = htmlParser.parseHTML(html_file_name)
-        print("HTML Input:")
-        print(html_input)
-        print("PDA Reader")
-        pda_file_name = input("Input PDA filename (.txt): ")
-        html_pda = pda.HTMLCheckerPDA()
-        html_pda.setPDA(*pda.txtPDAExtractor(pda_file_name))
-        result = html_pda.check_correctness(html_input)
-        if result == -1:
+        try:
+            print("""
+
+█▀▀ █░█ █▀▀ █▀▀ █▄▀   █░█ ▀█▀ █▀▄▀█ █░░
+█▄▄ █▀█ ██▄ █▄▄ █░█   █▀█ ░█░ █░▀░█ █▄▄
+                  
+                  """)
+            result = htmlParser.parseHTML()
             print("""
 ░ Result:
-░ >> HTML IS VALID                
-        """)
-        else:
-            print(f"""
-░ Result:
-░ >> HTML IS INVALID (error at line: {result})                
-        """)
-        print()
-        break 
+░ >> ACCEPTED                
+""")
+            if result:
+                for r in result:
+                    print(r)
+            print()
+            break 
+        except FileNotFoundError:
+            print("File not found. Please try again.") 
+            system('pause')
+            system('cls')
+        except Exception as e:
+            print(f"An error occurred: {e}") 
+            print(f"Chosen Menu: {chooseMenu}")
+            system('pause')
 
     elif (chooseMenu == '2') or (chooseMenu == 'HELP'):
-        while True:
-            print("""
-                  
-█░█ █▀█ █░█░█   ▀█▀ █▀█   █░█ █▀ █▀▀
-█▀█ █▄█ ▀▄▀▄▀   ░█░ █▄█   █▄█ ▄█ ██▄
-
-░ 1. 
-░ 2.
-░ 3. 
-                  
-░ Type 'back' to return to the main menu.
-""")
-
-            chooseHelp = str(input("░ ")).upper()
-            print(chooseHelp)
-            if chooseHelp == 'BACK':
-                system('cls')
-                break
-            else:
-                print("Invalid.")
-                system('cls')
-
+            while True:
+                print("""
+                    
+    █░█ █▀█ █░█░█   ▀█▀ █▀█   █░█ █▀ █▀▀
+    █▀█ █▄█ ▀▄▀▄▀   ░█░ █▄█   █▄█ ▄█ ██▄
+    ░ 1. 
+    ░ 2.
+    ░ 3. 
+                    
+    ░ Type 'back' to return to the main menu.
+    """)
+                chooseHelp = str(input("░ ")).upper()
+                print(chooseHelp)
+                if chooseHelp == 'BACK':
+                    system('cls')
+                    break
+                else:
+                    print("Invalid.")
+                    system('cls')
     elif (chooseMenu == '3') or (chooseMenu == 'EXIT'):
         system('cls')
         sys.exit()
-
     else:
-        print("Invalid.")        
-
+        print("Invalid.")
