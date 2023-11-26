29deb5195f602cb266e99b03789ccf54d3b6ed86
heisenberg <owentobias21@gmail.com>
2023-11-26 15:16:56 +0700

    clean up pda

--- src/pda.py
+++ src/pda.py
@@ -16,61 +16,6 @@ def txtPDAExtractor(filename):
         transition_functions = lines[7:]
     return states, input_symbols, stack_symbols, starting_state, starting_stack, accepting_states, accept_type, transition_functions 
 
-def checkHTML():
-    results = parseHTML()
-    for i in range(len(results)):
-        stacks, state = transition(lines, state, results[i][1], stacks)
-        print("Stacks:", stacks, "State:", state)
-        if state == False:
-            print(stacks)
-            if stacks[-1] in symbol:
-                print("Syntax error in line", results[i][0], "\nInvalid placement:", results[i][1], " inside tag ", '<',stacks[-2],'>', sep='')
-            else:
-                print("Syntax error in line", results[i][0], "\nUnrecognized word:", results[i][1])
-            system("pause")
-            system("cls")
-        elif state == True:
-            require = ['html', 'head', 'body']
-            results = [item for sublist in results for item in sublist]
-            results = ([element.replace('<', '').replace('>', '').replace('/', '') for element in results if isinstance(element, str) and not element.isdigit()])
-            print("results=",results)
-            print("stacks", stacks)
-            missing = [x for x in require if x not in stacks and x not in results]
-            print("missing", missing)
-            if len(missing) == 0:
-                print('''Check your tag nesting!
-Correct code:
-<html>
-    <head>
-    </head>
-    <body>
-    </body>
-</html>''')
-            else:
-                print("You're missing a required tag")
-                print("Missing tag: ", missing)
-            system("pause")
-            system("cls")
-        skip += 1
-
-    print("FS", state, stacks)
-
-    #FINISHING HANDLE
-    if len(stacks) == 0:
-        if state == 'F':
-            print("Accepted")
-        else:
-            print("NOT HANDLED state=", state, "stacks=", stacks)
-    else:
-        error = [x for x in results if x[1] == '<' + stacks[0] + '>' or x[1] == stacks[0]]
-        print("Syntax error in line", error[0][0])
-        if len(stacks) != len(set(stacks)):
-            duplicate = [item for item, count in Counter(stacks).items() if count > 1]
-            print("There is no closing tag for", (", ".join(str(item) for item in duplicate)))
-        else:
-            print("NOT HANDLED")
-
-
 class HTMLCheckerPDA:
     def __init__(self):
         self.states = []
@@ -163,6 +108,5 @@ class HTMLCheckerPDA:
         if (self.current_state in self.accepting_states) and not self.stack:
             return -1 # if html valid, it will return -1 because there is no line which the code is invalid
             
-
 
 
